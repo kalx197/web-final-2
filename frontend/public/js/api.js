52fd09198f49f51d56fd5395ad1ca12d3cc68452
kalx197 <kaleabteklemariam197@gmail.com>
2026-07-06 11:22:51 -0600

    Implement ApiClient class for API requests

---
+++ frontend/public/js/api.js
@@ -0,0 +1,29 @@
+const API_BASE = 'http://localhost:5000/api';
+
+class ApiClient {
+    constructor() {
+        this.token = localStorage.getItem('token');
+    }
+
+    setToken(token) {
+        this.token = token;
+        localStorage.setItem('token', token);
+    }
+
+    getHeaders() {
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+        if (this.token) {
+            headers['Authorization'] = `Bearer ${this.token}`;
+        }
+        return headers;
+    }
+
+    async request(endpoint, options = {}) {
+        const response = await fetch(`${API_BASE}${endpoint}`, {
+            ...options,
+            headers: this.getHeaders()
+        });
+
+        const data = await
