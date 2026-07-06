b9b6cf5c47ad482bbbe073a1cd0f3c2e6ae4b809
kalx197 <kaleabteklemariam197@gmail.com>
2026-07-06 11:07:08 -0600

    Initialize server and connect to database

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const app = require('./src/app');
+const { connectDB } = require('./src/config/database');
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
